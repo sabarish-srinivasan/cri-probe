741a13e50b3cb9301b2d81e93adb9a8e68f0315d
sabarish.srinivasan <srinivasansabarish@gmail.com>
2022-06-03 17:27:02 -0400

    Cri-probe autodetect WIP

--- criprobe/cri.py
+++ criprobe/cri.py
@@ -1,4 +1,6 @@
 import re
+import serial
+import serial.tools.list_ports
 
 
 class CriProbe:
@@ -11,4 +13,34 @@ class CriProbe:
             self.probes = [{'Type': 'CR-100'},
                            {'Type': 'CR-250'}]
         else:
-            self.probes = None
+            self.probes = []
+            ports = serial.tools.list_ports.comports()
+            for port in ports:
+                if re.search(r'A\d{6}', port.device):
+                    with serial.Serial(port.device, 115200, timeout=1) as cri_probe:
+                        probe_info = {}
+                        cri_probe.write(b'RC ID\r\n')
+                        probe_result = cri_probe.readline()
+                        m = re.search(r'(A\d{5})', str(probe_result))
+                        if m:
+                            reg = m.group(1)
+                            probe_id = str(reg)
+                        probe_info['ID'] = probe_id
+                        cri_probe.write(b'RC Model\r\n')
+                        m = re.search(r'(CR-\d{3})', str(probe_result))
+                        if m:
+                            reg = m.group(1)
+                            probe_model = str(reg)
+                        probe_info['Model'] = probe_model
+                        cri_probe.write(b'RC InstrumentType\r\n')
+                        m = re.search(r'(\d)', str(probe_result))
+                        if m:
+                            reg = m.group(1)
+                            if int(reg) == 0:
+                                probe_type = 'Photometer'
+                            elif int(reg) == 1:
+                                probe_type = 'Colorimeter'
+                            elif int(reg) == 2:
+                                probe_type = 'Spectroradiometer'
+                        probe_info['Type'] = probe_type
+                        self.probes.append(probe_info)
--- criprobe/tests/test_cri.py
+++ criprobe/tests/test_cri.py
@@ -1,5 +1,6 @@
 import unittest
 import criprobe as cri
+import re
 
 
 class MyTestCase(unittest.TestCase):
@@ -10,6 +11,21 @@ class MyTestCase(unittest.TestCase):
         # Check the simulated probes
         self.assertEqual(p.probes[0], {'Type': 'CR-100'})
         self.assertEqual(p.probes[1], {'Type': 'CR-250'})
+
+    def test_init_real_probe(self):
+        # this test will only be meaningful if real probes are connected
+        # if no probes are connected it will simply pass
+        p = cri.CriProbe()
+
+        # Only check results if there are real probes attached
+        if p.probes:
+            # check to make sure each probe in the list has valid info
+            for probe in p.probes:
+                self.assertTrue(re.search(r'A\d{6}', probe['ID']))
+            # Force a failure
+            # self.assertEqual(True, False)
+        else:
+            pass
 
 
 if __name__ == '__main__':
